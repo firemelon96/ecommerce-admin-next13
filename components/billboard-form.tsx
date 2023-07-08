e1665c67593c9d6a4854ce0929ccabf908d52273
estong jamion <estong.jamion@gmail.com>
2023-07-09 00:41:49 +0800

    Added the Billboard routes

--- components/billboard-form.tsx
+++ components/billboard-form.tsx
@@ -2,7 +2,7 @@
 
 import * as z from 'zod';
 import { useForm } from 'react-hook-form';
-import { Store } from '@prisma/client';
+import { Billboard } from '@prisma/client';
 import { Trash } from 'lucide-react';
 import { zodResolver } from '@hookform/resolvers/zod';
 import { useParams, useRouter } from 'next/navigation';
@@ -24,35 +24,54 @@ import { toast } from 'react-hot-toast';
 import { AlertModal } from '@/components/modals/alert-model';
 import { ApiAlert } from '@/components/ui/api-alert';
 import { useOrigin } from '@/hooks/use-origin';
-
-interface SettingsFormProps {
-  initialData: Store;
-}
+import ImageUpload from './ui/image-upload';
 
 const formSchema = z.object({
-  name: z.string().min(1),
+  label: z.string().min(1),
+  imageUrl: z.string().min(1),
 });
 
-type SettingsFormValues = z.infer<typeof formSchema>;
+type BillboardFormValues = z.infer<typeof formSchema>;
 
-export const SettingsForm: React.FC<SettingsFormProps> = ({ initialData }) => {
+interface BillboardFormProps {
+  initialData: Billboard | null;
+}
+
+export const BillboardForm: React.FC<BillboardFormProps> = ({
+  initialData,
+}) => {
   const params = useParams();
   const router = useRouter();
-  const origin = useOrigin();
+
   const [open, setOpen] = useState(false);
   const [loading, setLoading] = useState(false);
 
-  const form = useForm<SettingsFormValues>({
+  const title = initialData ? 'Edit' : 'Create billboard';
+  const description = initialData ? 'Edit a billboard' : 'Add a new billboard';
+  const toastMessage = initialData ? 'Billboard Updated' : 'Billboard Created';
+  const action = initialData ? 'Save changes' : 'Create';
+
+  const form = useForm<BillboardFormValues>({
     resolver: zodResolver(formSchema),
-    defaultValues: initialData,
+    defaultValues: initialData || {
+      label: '',
+      imageUrl: '',
+    },
   });
 
-  const onSubmit = async (data: SettingsFormValues) => {
+  const onSubmit = async (data: BillboardFormValues) => {
     try {
       setLoading(true);
-      await axios.patch(`/api/stores/${params.storeId}`, data);
+      if (initialData) {
+        await axios.patch(
+          `/api/${params.storeId}/billboards/${params.billboardId}`,
+          data
+        );
+      } else {
+        await axios.post(`/api/${params.storeId}/billboards`, data);
+      }
       router.refresh();
-      toast.success('Store updated');
+      toast.success(toastMessage);
     } catch (error) {
       toast.error('Something went wrong');
     } finally {
@@ -63,12 +82,14 @@ export const SettingsForm: React.FC<SettingsFormProps> = ({ initialData }) => {
   const onDelete = async () => {
     try {
       setLoading(true);
-      await axios.delete(`/api/stores/${params.storeId}`);
+      await axios.delete(
+        `/api/${params.storeId}/billboards/${params.billboardId}`
+      );
       router.refresh();
       router.push('/');
-      toast.success('Store deleted');
+      toast.success('Billboard deleted');
     } catch (error) {
-      toast.error('Make sure to remove all products and categories first.');
+      toast.error('Make sure to remove all categories using this billboard.');
     } finally {
       setLoading(false);
       setOpen(false);
@@ -84,15 +105,18 @@ export const SettingsForm: React.FC<SettingsFormProps> = ({ initialData }) => {
         loading={loading}
       />
       <div className='flex items-center justify-between'>
-        <Heading title='Settings' description='Manage store preferences' />
-        <Button
-          disabled={loading}
-          variant='destructive'
-          size='sm'
-          onClick={() => setOpen(true)}
-        >
-          <Trash className='h-4 w-4' />
-        </Button>
+        <Heading title={title} description={description} />
+
+        {initialData && (
+          <Button
+            disabled={loading}
+            variant='destructive'
+            size='sm'
+            onClick={() => setOpen(true)}
+          >
+            <Trash className='h-4 w-4' />
+          </Button>
+        )}
       </div>
       <Separator />
       <Form {...form}>
@@ -100,17 +124,34 @@ export const SettingsForm: React.FC<SettingsFormProps> = ({ initialData }) => {
           onSubmit={form.handleSubmit(onSubmit)}
           className='space-y-8 w-full'
         >
+          <FormField
+            control={form.control}
+            name='imageUrl'
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Background image</FormLabel>
+                <FormControl>
+                  <ImageUpload
+                    value={field.value ? [field.value] : []}
+                    disabled={loading}
+                    onChange={(url) => field.onChange(url)}
+                    onRemove={() => field.onChange('')}
+                  />
+                </FormControl>
+              </FormItem>
+            )}
+          />
           <div className='grid grid-cols-3 gap-8'>
             <FormField
               control={form.control}
-              name='name'
+              name='label'
               render={({ field }) => (
                 <FormItem>
                   <FormLabel>Name</FormLabel>
                   <FormControl>
                     <Input
                       disabled={loading}
-                      placeholder='Store name'
+                      placeholder='Billboard label'
                       {...field}
                     />
                   </FormControl>
@@ -119,16 +160,11 @@ export const SettingsForm: React.FC<SettingsFormProps> = ({ initialData }) => {
             />
           </div>
           <Button disabled={loading} className='ml-auto' type='submit'>
-            Save Changes
+            {action}
           </Button>
         </form>
       </Form>
       <Separator />
-      <ApiAlert
-        title='NEXT_PUBLIC_API_URL'
-        description={`${origin}/api/${params.storeId}`}
-        variant='public'
-      />
     </>
   );
 };
