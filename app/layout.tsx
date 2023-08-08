992db1148510cf6c7d57e3b050845410fdcf29f9
estong jamion <estong.jamion@gmail.com>
2023-08-08 13:22:30 +0300

    Added darkmode toggle

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,8 @@ import { ClerkProvider } from '@clerk/nextjs';
 import { ModalProvider } from '@/providers/modal-provider';
 import { ToastProvider } from '@/providers/toast-provider';
 
+import { ThemeProvider } from '@/providers/theme-provider';
+
 import './globals.css';
 
 const inter = Inter({ subsets: ['latin'] });
@@ -22,9 +24,11 @@ export default function RootLayout({
     <ClerkProvider>
       <html lang='en'>
         <body className={inter.className}>
-          <ToastProvider />
-          <ModalProvider />
-          {children}
+          <ThemeProvider attribute='class' defaultTheme='system' enableSystem>
+            <ToastProvider />
+            <ModalProvider />
+            {children}
+          </ThemeProvider>
         </body>
       </html>
     </ClerkProvider>
